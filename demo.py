82694bdbf3ae8d30d0b592f06aa6dac23abd310a
Jonah Hamilton <jonah.hamilton@gmail.com>
2022-11-17 13:42:50 -0800

    adding optional positional argument

--- demo.py
+++ demo.py
@@ -5,7 +5,8 @@
 Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
-<arg1>             Takes any value (this is a required positional argument)
+<arg1>            Takes any value (this is a required positional argument)
+[<arg20>]         Takes any value (this is a optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 """ 
