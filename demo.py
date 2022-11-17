e10ebbfa899829c146b69fd863aaa9056871df54
Jonah Hamilton <jonah.hamilton@gmail.com>
2022-11-17 13:45:49 -0800

    adding optional positional argument to usage

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@
 # date: 2020-01-15
 
 """This script prints out docopt args.
-Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.py <arg1> [<arg20>] --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
 <arg1>            Takes any value (this is a required positional argument)
